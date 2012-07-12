fe908ee8a361874d5edf6c307acd32198f5bdf82
sonicxml <sonicxml@gmail.com>
2012-07-12 09:11:23 -0400

    Initial Changes

--- products/common.mk
+++ products/common.mk
@@ -1,6 +1,6 @@
 # Generic product
-PRODUCT_NAME := FruitsAndVeggies
-PRODUCT_BRAND := FruitsAndVeggies
+PRODUCT_NAME := Vanir
+PRODUCT_BRAND := Vanir
 PRODUCT_DEVICE := generic
 
 # Build Properties
@@ -20,40 +20,91 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Build.Prop Tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0 \
-    windowsmgr.max_events_per_sec=240 \
-    debug.performance.tuning=1 \
-    video.accelerate.hw=1 \
-    debug.sf.hw=1 \
-    ro.media.enc.jpeg.quality=100 \
-    pm.sleep_mode=1 \
-    ro.ril.disable.power.collapse=0 \
-    ro.telephony.call_ring.delay=0 \
-    video.accelerate.hw=1 \
-    ro.kernel.checkjni=0 \
-    net.tcp.buffersize.default=4096,87380,256960,4096,16384,256960 \
+    wifi.supplicant_scan_interval=300 \
+    net.bt.name=Android \
+    dalvik.vm.stack-trace-file=/data/anr/traces.txt \
+    dalvik.vm.heapstartsize=48m \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=384m \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.dexopt-flags=v=a,o=v,u=y,m=y \
+    dalvik.vm.lockprof.threshold=75 \
+    dalvik.vm.verify-bytecode=false \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+    ro.gsm.data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
+    ro.gsm.2nd_data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
+    ro.cdma.otaspnumschema=SELC,1,80,99 \
+    ro.ril.enable.3g.prefix=1 \
+    ro.ril.hep=1 \
+    ro.ril.hsxpa=2 \
+    ro.ril.enable.dtm=1 \
+    ro.ril.gprsclass=12 \
+    ro.ril.hsdpa.category=8 \
+    ro.ril.enable.a53=1 \
+    ro.ril.hsupa.category=5 \
+    net.tcp.buffersize.default=4096,87380,256960,4096,16384,256960  \
     net.tcp.buffersize.wifi=4096,87380,256960,4096,16384,256960 \
     net.tcp.buffersize.umts=4096,87380,256960,4096,16384,256960 \
     net.tcp.buffersize.gprs=4096,87380,256960,4096,16384,256960 \
-    net.tcp.buffersize.edge=4096,87380,256960,4096,16384,256960 
-
+    net.tcp.buffersize.edge=4096,87380,256960,4096,16384,256960 \
+    ro.kernel.android.checkjni=0 \
+    ro.media.enc.jpeg.quality=100 \
+    debug.sf.hw=1 \
+    debug.performance.tuning=1 \
+    video.accelerate.hw=1 \
+    persist.sys.purgeable_assets=1 \
+    windowsmgr.max_events_per_sec=280 \
+    pm.sleep_mode=1 \
+    ro.HOME_APP_ADJ=1 \
+    persist.sys.use_dithering=0 \
+    ro.ril.disable.power.collapse=0 \
+    ro.media.dec.jpeg.memcap=8000000 \
+    ro.media.enc.hprof.vid.bps=8000000 \
+    ro.media.enc.hprof.vid.fps=65 \
+    ro.config.nocheckin=1 \
+    ro.mot.eri.losalert.delay=1000 \
+    ro.config.vc_call_vol_steps=42 \
+    ring.delay=0 \
+    ro.telephony.call_ring.delay=500 \
+    ro.lge.proximity.delay=20 \
+    mot.proximity.delay=20 \
+    ro.config.hwfeature_wakeupkey=0 \
+    ro.ext4fs=1 \
+    debug.composition.type=gpu \
+    ro.mot.eri.losalert.delay=900 \
+    ro.ril.fast.dormancy.rule=0 \
+    ro.HOME_APP_MEM=8192 \
+    ro.FOREGROUND_APP_MEM=6144 \
+    ro.VISIBLE_APP_MEM=8192 \
+    ro.max.fling_velocity=15000 \
+    ro.min.fling_velocity=10000 \
+    movfilter=40 \
+    move hyst=0 \
+    per_sec=300 \
+    TCHTHR=28 \
+    ro.semc.xloud.supported=true \
+    persist.service.xloud.enable=1 \
+    ro.semc.sound_effects_enabled=true \
+    ro.service.swiqi.supported=true \
+    persist.service.swiqi.enable=1
 
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRN84D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
 
 # Blobs common to all devices
 PRODUCT_COPY_FILES += \
-    vendor/fnv/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \
-    vendor/fnv/proprietary/common/xbin/busybox:system/xbin/busybox \
-    vendor/fnv/proprietary/common/xbin/su:system/xbin/su \
-    vendor/fnv/proprietary/common/xbin/sysrw:system/xbin/sysrw \
-    vendor/fnv/proprietary/common/xbin/sysro:system/xbin/sysro 
+    vendor/vanir/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \
+    vendor/vanir/proprietary/common/xbin/busybox:system/xbin/busybox \
+    vendor/vanir/proprietary/common/xbin/su:system/xbin/su \
+    vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
+    vendor/vanir/proprietary/common/xbin/sysro:system/xbin/sysro 
 
 
 # Misc Files
 PRODUCT_COPY_FILES +=  \
-vendor/fnv/proprietary/common/etc/hosts:system/etc/hosts \
-vendor/fnv/proprietary/common/etc/resolv.conf:system/etc/resolv.conf 
+vendor/vanir/proprietary/common/etc/hosts:system/etc/hosts \
+vendor/vanir/proprietary/common/etc/resolv.conf:system/etc/resolv.conf 
 
 # Audio Packages
 include frameworks/base/data/sounds/AudioPackage7.mk
--- products/toro.mk
+++ products/toro.mk
@@ -2,33 +2,33 @@
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/fnv/products/common.mk)
+$(call inherit-product, vendor/vanir/products/common.mk)
 
 # Setup device specific product configuration.
-PRODUCT_NAME := fnv_toro
+PRODUCT_NAME := Vanir_toro
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := toro
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_ID=ICL53F BUILD_FINGERPRINT=google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys" BUILD_NUMBER=235179
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_ID=JRN84D BUILD_FINGERPRINT=google/mysid/toro:4.1.1/JRN84D/336647:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRN84D 336647 release-keys" BUILD_NUMBER=235179
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
 # Blobs necessary for drm
 PRODUCT_COPY_FILES +=  \
-   vendor/fnv/proprietary/common/lib/libfrsdk.so:system/lib/libfrsdk.so \
-   vendor/fnv/proprietary/common/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-   vendor/fnv/proprietary/common/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-   vendor/fnv/proprietary/common/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-   vendor/fnv/proprietary/common/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so 
+   vendor/vanir/proprietary/common/lib/libfrsdk.so:system/lib/libfrsdk.so \
+   vendor/vanir/proprietary/common/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+   vendor/vanir/proprietary/common/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+   vendor/vanir/proprietary/common/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+   vendor/vanir/proprietary/common/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so 
 
 # Copy toro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/fnv/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/fnv/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/fnv/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
+    vendor/vanir/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/vanir/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/vanir/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
 
 
 
