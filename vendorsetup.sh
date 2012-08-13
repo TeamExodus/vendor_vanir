246c395ab9d67488d781b1dcd20d691d1872952f
sonicxml <sonicxml@gmail.com>
2012-08-12 22:10:17 -0400

    Building user blocks adb root access unless you adb shell, so let's build userdebug

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo vanir_toro-user
-add_lunch_combo vanir_maguro-user
-add_lunch_combo vanir_toroplus-user
+add_lunch_combo vanir_toro-userdebug
+add_lunch_combo vanir_maguro-userdebug
+add_lunch_combo vanir_toroplus-userdebug
