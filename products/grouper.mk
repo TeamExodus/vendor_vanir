6b1e318acacc3240ac9243f1a3f92dfd9fd9aa99
dho <dho@dho.im>
2012-10-12 14:43:23 -0400

    Bump Grouper #
    Change-Id: Ibdb8e58e65787900a0f0d36abd0818d86cdf0679

--- products/grouper.mk
+++ products/grouper.mk
@@ -13,7 +13,7 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1.1/JRO03H/405518:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03H 405518 release-keys" BUILD_NUMBER=405518
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1.1/JZO54K/405518:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JZO54K 405518 release-keys" BUILD_NUMBER=405518
 
 # Need custom boot in proper resolution
 
