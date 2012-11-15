0b5e4e7196969575398bd1befe845c755177432a
nuclearmistake <nuclearmistake@gmail.com>
2012-11-15 15:50:18 -0500

    make numbers bigger
    Change-Id: I02c4d42ae3c157d9626c19fb0b57fbfa298bbe52

--- products/toroplus.mk
+++ products/toroplus.mk
@@ -18,7 +18,7 @@ PRODUCT_DEVICE := toroplus
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysidspr BUILD_FINGERPRINT=google/mysidspr/toroplus:4.1.2/JOP40C/336647:user/release-keys PRIVATE_BUILD_DESC="mysidspr-user 4.1.2 JOP40C 336647 release-keys" BUILD_NUMBER=235179
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysidspr BUILD_FINGERPRINT=google/mysidspr/toroplus:4.2/JOP40C/336647:user/release-keys PRIVATE_BUILD_DESC="mysidspr-user 4.2 JOP40C 336647 release-keys" BUILD_NUMBER=235179
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
