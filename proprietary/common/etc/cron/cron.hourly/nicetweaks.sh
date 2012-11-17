188e3d61fcec72f986113b762797cff4ffa22860
nuclearmistake <nuclearmistake@gmail.com>
2012-11-17 08:43:14 -0500

    Renice in a way that should stick better
    Change-Id: If51e27ecf6c73bb7b04992f40ceb6acdbac3d919

--- proprietary/common/etc/cron/cron.hourly/nicetweaks.sh
+++ proprietary/common/etc/cron/cron.hourly/nicetweaks.sh
@@ -5,12 +5,12 @@
 #
 # Nice Tweaks
 #
-busybox renice -15 $(pidof com.android.launcher)
-busybox renice -12 $(pidof com.android.chrome)
-busybox renice -12 $(pidof com.google.android.browser)
-busybox renice -11 $(pidof com.google.android.googlequicksearchbox)
-busybox renice -10 $(pidof com.android.phone)
-busybox renice -10 $(pidof com.android.systemui)
-busybox renice -7 $(pidof com.google.android.inputmethod.latin)
-busybox ionice -c2 -n0 $(pidof com.android.camera)
-busybox ionice -c1 -n1 $(pidof com.google.android.music)
+renice -15 $(pidof com.android.launcher)
+renice -12 $(pidof com.android.chrome)
+renice -12 $(pidof com.google.android.browser)
+renice -11 $(pidof com.google.android.googlequicksearchbox)
+renice -10 $(pidof com.android.phone)
+renice -10 $(pidof com.android.systemui)
+renice -7 $(pidof com.google.android.inputmethod.latin)
+ionice $(pidof com.android.camera) be 0
+ionice $(pidof com.google.android.music) rt 1
