65eaf435d562363a03a6ec33410ec870b1f0608e
nuclearmistake <nuclearmistake@gmail.com>
2012-11-18 10:55:31 -0500

    Renice more stuff
    Renice any of a few 3rd party keyboards (user apps don't get pids until they run... hopefully the default one gets it before then.)
    Renice any of a few launchers (matched with wildcard... only the running one has a pid)
    
    Change-Id: I0c955288088c54833a5fb2b8a8e51696d89e9a90

--- products/common.mk
+++ products/common.mk
@@ -95,7 +95,8 @@ PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/vanirtweakapply:system/xbin/vanirtweakapply \
     vendor/vanir/proprietary/common/xbin/vanir:system/xbin/vanir \
     vendor/vanir/proprietary/common/xbin/vanirflash:system/xbin/vanirflash \
-    vendor/vanir/proprietary/common/xbin/busybox:system/xbin/busybox
+    vendor/vanir/proprietary/common/xbin/busybox:system/xbin/busybox \
+    vendor/vanir/proprietary/common/xbin/vanirnice:system/xbin/banirnice
 
 
 # Misc Files
--- proprietary/common/etc/cron/cron.hourly/nicetweaks.sh
+++ proprietary/common/etc/cron/cron.hourly/nicetweaks.sh
@@ -5,12 +5,4 @@
 #
 # Nice Tweaks
 #
-renice -15 $(pidof com.android.launcher)
-renice -12 $(pidof com.android.chrome)
-renice -12 $(pidof com.google.android.browser)
-renice -11 $(pidof com.google.android.googlequicksearchbox)
-renice -10 $(pidof com.android.phone)
-renice -10 $(pidof com.android.systemui)
-renice -7 $(pidof com.google.android.inputmethod.latin)
-ionice $(pidof com.android.camera) be 0
-ionice $(pidof com.google.android.music) rt 1
+vanirnice
