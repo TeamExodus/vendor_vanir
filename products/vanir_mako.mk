d6184d319f96c62072be1a63aaa80e5f696927a6
nuclearmistake <nuclearmistake@gmail.com>
2013-02-27 08:42:06 -0500

    Stripped down props for sanity
    Change-Id: I7001b67e112c6aa4842a3fa4331b52545a7c7d71

--- products/vanir_mako.mk
+++ products/vanir_mako.mk
@@ -1,20 +1,9 @@
 #Squisher Choosing
 DHO_VENDOR := vanir
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true \
-    ro.config.vc_call_vol_steps=7 \
-    ring.delay=0 \
-    ro.telephony.call_ring.delay=50 \
-    ro.ril.fast.dormancy.rule=0 \
-    ro.goo.rom=vanir-MAKO
-
 # place boot animation here once we make one
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/mako/media/bootanimation.zip:system/media/bootanimation.zip
-
-# Vanir configuration
-$(call inherit-product, vendor/vanir/products/gsm.mk)
 
 # Inherit AOSP device configuration for mako.
 $(call inherit-product, device/lge/mako/full_mako.mk)
@@ -53,79 +42,22 @@ PRODUCT_PACKAGES += \
     DSPManager \
     Email
 
-# Build Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
-    ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.error.receiver.system.apps=com.google.android.feedback \
-    ro.com.google.locationfeatures=1 \
-    ro.setupwizard.mode=OPTIONAL \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.config.ringtone=Hydra.ogg \
-    ro.config.notification_sound=Proxima.ogg \
-    ro.config.alarm_alert=Alarm_Beep_03.ogg \
-    ro.goo.version=4.02 \
-    ro.modversion=v4.002-beta
-
 # Build.Prop Tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.checkjni=false \
     dalvik.vm.dexopt-flags=m=y \
     dalvik.vm.execution-mode=int:jit \
-    dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapsize=384m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.lockprof.threshold=850 \
     dalvik.vm.stack-trace-file=/data/anr/traces.txt \
     dalvik.vm.verify-bytecode=false \
-    mot.proximity.delay=20 \
-    movfilter=40 \
-    per_sec=300 \
-    net.bt.name=Android \
-    persist.service.swiqi.enable=1 \
-    persist.service.xloud.enable=1 \
     persist.sys.purgeable_assets=1 \
     persist.sys.use_dithering=0 \
     pm.sleep_mode=1 \
     ro.config.nocheckin=1 \
     ro.config.hwfeature_wakeupkey=0 \
-    ro.ext4fs=1 \
-    ro.goo.developerid=vanir \
-    ro.HOME_APP_ADJ=1 \
-    ro.HOME_APP_MEM=8192 \
-    ro.FOREGROUND_APP_MEM=8192 \
     ro.kernel.android.checkjni=0 \
     ro.kernel.checkjni=0 \
-    ro.lge.proximity.delay=20 \
-    ro.max.fling_velocity=15000 \
-    ro.media.enc.hprof.vid.bps=8000000 \
-    ro.media.enc.hprof.vid.fps=65 \
-    ro.media.dec.jpeg.memcap=8000000 \
-    ro.media.enc.jpeg.quality=100 \
-    ro.min.fling_velocity=10000 \
-    ro.mot.eri.losalert.delay=600 \
-    ro.semc.xloud.supported=true \
-    ro.semc.sound_effects_enabled=true \
-    ro.service.swiqi.supported=true \
-    ro.rommanager.developerid=vanir \
-    ro.VISIBLE_APP_MEM=8192 \
-    video.accelerate.hw=1 \
     wifi.supplicant_scan_interval=300 \
     windowsmgr.max_events_per_sec=280
-
-### Possible culprits in random reboots I THINK?... difference is negligible without these
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.composition.type=gpu
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.performance.tuning=1
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.hw=1
-
-### These will prevent any FC popups from showing... ever. (lol)
-#    profiler.force_disable_err_rpt=1 \
-#    profiler.force_disable_ulog=1 \
 
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JDQ39 BUILD_ID=JDQ39 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
