8791a4a6a2de4b914194c9dd697319599b7b80c2
nuclearmistake <nuclearmistake@gmail.com>
2013-04-27 17:14:27 -0400

    JDQ39E
    Change-Id: I3d8c957404779306727729f764b05df189a78a3b

--- products/vanir_manta.mk
+++ products/vanir_manta.mk
@@ -63,7 +63,7 @@ PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2.2 JDQ39 573038 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.2.2/JDQ39E/573038:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2.2 JDQ39E 573038 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=Vanir-MANTA
