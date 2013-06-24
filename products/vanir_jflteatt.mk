7426fefaa125d3c49525f0add3b356278d19ef03
nuclearmistake <nuclearmistake@gmail.com>
2013-06-24 14:52:08 -0400

    Fix copy/pasta-related booboo
    Change-Id: I68eb4d2a3aa111da17db06149fd1e73506506f7d

--- products/vanir_jflteatt.mk
+++ products/vanir_jflteatt.mk
@@ -16,4 +16,4 @@ $(call inherit-product, vendor/vanir/products/gsm.mk)
 PRODUCT_NAME := vanir_jflteatt
 PRODUCT_DEVICE := jflteatt
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
