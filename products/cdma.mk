7d3cd1b5945c936c8d9b0727bbd4ee907900851a
nuclearmistake <nuclearmistake@gmail.com>
2013-07-17 18:33:24 -0400

    Remove Stk and spn-conf.xml from cdma.mk... and don't set that gsm phone prop to 2\!
    Change-Id: I3521e9a17afe7dbde19094c890e5fd797091cc61

--- products/cdma.mk
+++ products/cdma.mk
@@ -2,16 +2,5 @@
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/cdma/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# CDMA SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/cdma/etc/spn-conf.xml:system/etc/spn-conf.xml
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    gsm.current.phone-type=2
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
-
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/vanir/overlay/cdma_phone
