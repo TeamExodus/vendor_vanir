7fd7c1224d0a544b9fd00277df518ab20898b6d3
DHOMD <DHO@dho.im>
2013-08-07 19:58:30 -0400

    Remove outdated face unlock blob
    File was previously deleted, this is cleanup
    
    Change-Id: I00439fd7c091a647486a909c957eaed643c0ae00

--- products/vanir_manta.mk
+++ products/vanir_manta.mk
@@ -18,7 +18,6 @@ PRODUCT_PACKAGES += \
 
 # Blobs necessary for drm
 PRODUCT_COPY_FILES += \
-   vendor/vanir/proprietary/common/lib/libfrsdk.so:system/lib/libfrsdk.so \
    vendor/vanir/proprietary/common/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
    vendor/vanir/proprietary/common/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
    vendor/vanir/proprietary/common/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
