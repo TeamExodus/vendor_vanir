282ee8042871a5858beceb4b7c49d4cb0d4a4da0
PrimeDirective <activethrasher00@gmail.com>
2013-08-10 08:34:33 -0400

    FLO: use higher resolution boot animation from S4
    Change-Id: I3b820b0675bcef9b709dd7e269eef9b33f854160

--- products/vanir_flo.mk
+++ products/vanir_flo.mk
@@ -10,10 +10,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Tablet Overlays no radios
     PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_no_radio
 
-# Copy grouper specific prebuilt files
 ## FIX ME: bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/smalltab/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/jf/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Inherit common product files.
 $(call inherit-product, vendor/vanir/products/common_tabs.mk)
