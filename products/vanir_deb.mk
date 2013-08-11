1df8b2ea00839a84f1ab2faacd86a35c7b0771a7
PrimeDirective <activethrasher00@gmail.com>
2013-08-10 22:54:54 -0400

    add N7 deb variant
    Change-Id: Ie9574bfee18a876ed0da7eea8aab54296851267d

---
+++ products/vanir_deb.mk
@@ -0,0 +1,34 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+KERNEL_SPAM := jassy-release6
+
+# Run these first or they will not stick
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-FLO
+
+# Tablet Overlays no radios
+    PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_no_radio
+
+## FIX ME: bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/jf/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Inherit common product files.
+$(call inherit-product, vendor/vanir/products/common_tabs.mk)
+
+# Inherit AOSP device configuration for grouper
+$(call inherit-product, device/asus/deb/full_deb.mk)
+
+# Default values used by 99vanir. copied to /sdcard on first run, and presently it lives there forever... todo: make a runonce script that adds new default values to the copy on the sdcard as we add them or something
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/common/etc/vanir.cfg:system/etc/vanir.cfg
+
+# Setup device specific product configuration.
+PRODUCT_NAME := vanir_deb
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := deb
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor TARGET_DEVICE=flo BUILD_FINGERPRINT=google/razor/flo:4.3/JSS15J/748593:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.3 JSS15J 748593 release-keys"
