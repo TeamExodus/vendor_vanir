819bcae354b81c4bb62a500d0dd4b27f6e52ed36
nuclearmistake <nuclearmistake@gmail.com>
2013-08-20 15:09:15 -0400

    Bravia - handle prebuilts correctly
    Change-Id: Ic37a948ff32bc6d3fad8c3cc40b09c80c61c8dfc

--- products/media_sexificators.mk
+++ products/media_sexificators.mk
@@ -12,20 +12,23 @@ PRODUCT_COPY_FILES += \
 endif
 
 # Bravia 2 engine
+
+PRODUCT_PACKAGES += \
+    libiwi \
+    libiwiOmx \
+    libiwiOmxUtil \
+    libmbe \
+    libmbe_paramselector \
+    libSwiqiController \
+    libswiqisettinghelper
+
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/tuna/etc/be_movie:system/etc/be_movie \
-    vendor/vanir/proprietary/tuna/etc/be_movie_setting:system/etc/be_movie_setting \
-    vendor/vanir/proprietary/tuna/etc/be2_album:system/etc/be2_album \
-    vendor/vanir/proprietary/tuna/etc/be_album:system/etc/be_album \
-    vendor/vanir/proprietary/tuna/etc/permissions/com.sonyericsson.android.SwIqiBmp.xml:system/etc/permissions/com.sonyericsson.android.SwIqiBmp.xml \
-    vendor/vanir/proprietary/tuna/lib/libiwi.so:system/lib/libiwi.so \
-    vendor/vanir/proprietary/tuna/lib/libiwiOmx.so:system/lib/libiwiOmx.so \
-    vendor/vanir/proprietary/tuna/lib/libiwiOmxUtil.so:system/lib/libiwiOmxUtil.so \
-    vendor/vanir/proprietary/tuna/lib/libmbe.so:system/lib/libmbe.so \
-    vendor/vanir/proprietary/tuna/lib/libmbe_paramselector.so:system/lib/libmbe_paramselector.so \
-    vendor/vanir/proprietary/tuna/lib/libSwiqiController.so:system/lib/libSwiqiController.so \
-    vendor/vanir/proprietary/tuna/lib/libswiqisettinghelper.so:system/lib/libswiqisettinghelper.so \
-    vendor/vanir/proprietary/tuna/framework/com.sonyericsson.android.SwIqiBmp.jar:system/framework/com.sonyericsson.android.SwIqiBmp.jar
+    vendor/vanir/proprietary/bravia/etc/be_movie:system/etc/be_movie \
+    vendor/vanir/proprietary/bravia/etc/be_movie_setting:system/etc/be_movie_setting \
+    vendor/vanir/proprietary/bravia/etc/be2_album:system/etc/be2_album \
+    vendor/vanir/proprietary/bravia/etc/be_album:system/etc/be_album \
+    vendor/vanir/proprietary/bravia/etc/permissions/com.sonyericsson.android.SwIqiBmp.xml:system/etc/permissions/com.sonyericsson.android.SwIqiBmp.xml \
+    vendor/vanir/proprietary/bravia/framework/com.sonyericsson.android.SwIqiBmp.jar:system/framework/com.sonyericsson.android.SwIqiBmp.jar
 
 # Build.Prop Tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
