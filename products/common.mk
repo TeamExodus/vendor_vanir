b66a7e0fea36926e574bfcb32d798ecbee4943e0
nuclearmistake <nuclearmistake@gmail.com>
2013-09-07 16:41:02 -0400

    Maybe the mmac_permissions spam at early boot is from not having this?
    (from CM)
    
    Change-Id: I229343f9c7f756922114dce915b14cd4f4fe51d5

--- products/common.mk
+++ products/common.mk
@@ -227,3 +227,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+
+-include vendor/vanir/sepolicy/sepolicy.mk
