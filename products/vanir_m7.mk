4ad0fc8f6dc5be24ea65e3e4522919780cb6c650
Trevin Gandhi <sonicxml@gmail.com>
2013-09-11 18:25:58 -0400

    Don't need this right now
    Change-Id: I7d8d387a064f9c7ad3cb5f361a16d353280fb2e8

--- products/vanir_m7.mk
+++ products/vanir_m7.mk
@@ -12,6 +12,3 @@ $(call inherit-product, vendor/vanir/products/beats.mk)
 # Inherit common phone stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
-# Default values used by 99vanir. copied to /sdcard on first run, and presently it lives there forever... todo: make a runonce script that adds new default values to the copy on the sdcard as we add them or something
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/jf/etc/vanir.cfg:system/etc/vanir.cfg
