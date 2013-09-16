fbae140887feb735e8f6d857ee01cd5b0c117d91
DHOMD <DHO@dho.im>
2013-09-16 15:54:37 -0400

    Optimus G updates
    Change-Id: I3110c3c2dcf7c1e18a876f58d902d9647c6b0a60

--- products/vanir_e975.mk
+++ products/vanir_e975.mk
@@ -18,7 +18,7 @@ $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/gsm.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
 
-# Inherit AOSP device configuration for mako.
+# Inherit AOSP device configuration for Optimus G.
 $(call inherit-product, device/lge/e975/e975.mk)
 
 # Default values used by 99vanir. copied to /sdcard on first run, and presently it lives there forever... todo: make a runonce script that adds new default values to the copy on the sdcard as we add them or something
@@ -33,4 +33,4 @@ PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc_open_eu BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.1.2/JZO54K/E97510b.1360741472:user/release-keys PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.1.2 JZO54K E97510b.1360741472 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc_open_eu BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.3/JSS15Q/E97510b.1360741472:user/release-keys PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.3 JSS15Q E97510b.1360741472 release-keys"
--- products/vanir_e980.mk
+++ products/vanir_e980.mk
@@ -18,9 +18,9 @@ $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/gsm.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
 
-# Inherit AOSP device configuration for mako.
+# Inherit AOSP device configuration for Optimus G Pro.
 $(call inherit-product, device/lge/e980/full_e980.mk)
-7
+
 # Default values used by 99vanir. copied to /sdcard on first run, and presently it lives there forever... todo: make a runonce script that adds new default values to the copy on the sdcard as we add them or something
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/mako-vanir.cfg:system/etc/vanir.cfg
@@ -33,4 +33,4 @@ PRODUCT_MODEL := Optimus G Pro
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=>geefhd_open_eu BUILD_FINGERPRINT=lge/geefhd_open_eu/geefhd:4.1.2/JZO54K/E98610b.1373081564:user/release-keys PRIVATE_BUILD_DESC="geefhd_open_eu-user 4.1.2 JZO54K E98610b.1373081564 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=>geefhd_open_eu BUILD_FINGERPRINT=lge/geefhd_open_eu/geefhd:4.3/JSS15Q/E98610b.1373081564:user/release-keys PRIVATE_BUILD_DESC="geefhd_open_eu-user 4.3 JSS15Q E98610b.1373081564 release-keys"
