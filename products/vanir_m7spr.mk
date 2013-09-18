a0893526a53ce6fab823f04e81b53f9a676cb38a
DHOMD <DHO@dho.im>
2013-09-18 19:17:33 -0400

    Sprint is cdma
    Barely
    
    Change-Id: Ibc37e486bb930d118f5702b3f9246b9744e809ab

--- products/vanir_m7spr.mk
+++ products/vanir_m7spr.mk
@@ -6,7 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-m7spr
 
 # Inherit common vanir files.
-$(call inherit-product, vendor/vanir/products/gsm.mk)
+$(call inherit-product, vendor/vanir/products/cdma.mk)
 
 # Inherit model specific configuration.
 $(call inherit-product, vendor/vanir/products/vanir_m7.mk)
