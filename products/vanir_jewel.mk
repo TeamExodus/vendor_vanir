39083fc6aceb73be14922dc4df4da9dee3d8d869
DHOMD <DHO@dho.im>
2013-09-21 18:47:13 -0400

    Fix some deep down derps on Jewel
    Change-Id: Icaf379952374fdc306a40c57e2a6434b8a946cd2

--- products/vanir_jewel.mk
+++ products/vanir_jewel.mk
@@ -5,17 +5,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
     ro.goo.rom=vanir-jewel
 
+# Inherit common phone stuff
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+# Inherit common vanir files.
+$(call inherit-product, vendor/vanir/products/cdma.mk)
+$(call inherit-product, vendor/vanir/products/beats.mk)
+
+# Vanir Product Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/vanir/overlay/cdma_phone
+    vendor/vanir/proprietary/boot_animations/800x1280.zip:system/media/bootanimation.zip 
+
 # Inherit AOSP device configuration for toro.
 $(call inherit-product, device/htc/jewel/jewel.mk)
 
-# Boot animation
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/800x1280.zip:system/media/bootanimation.zip 
-
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/vanir/overlay/cdma_phone
-
-#... and then make the AOSP device configuration for toro get on its knees and sing the national anthem into our "microphones"
+#... and then make the AOSP device configuration for Jewel get on its knees and sing the national anthem into our "microphones"
 PRODUCT_LOCALES := en_US
 
 # Setup device specific product configuration.
