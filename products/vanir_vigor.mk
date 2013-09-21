ebb44fb823acec2ea4e67c5e2fca1d1dea1d5d63
Snuzzo <jrracinfan@gmail.com>
2013-09-21 08:26:35 -0400

    vigor: Add vanir.cfg
    PS2:
    herp derp, vigor not toro! Silly me! :)
    
    PS1:
    Going to start working on adding VanirTweaks support.
    Already have LunarTools initd & command line support from vendor/htc/vigor which will be removed once full control is established for this device.
    
    Change-Id: I7c1401dc360b582064785991e053d340d7f3241d

--- products/vanir_vigor.mk
+++ products/vanir_vigor.mk
@@ -5,24 +5,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
     ro.goo.rom=vanir-vigor
 
-# Inherit AOSP device configuration for toro.
+# Inherit AOSP device configuration for vigor.
 $(call inherit-product, device/htc/vigor/vigor.mk)
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
 
 # Boot animation
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+	vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
 
 # CDMA APN list
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/cdma/etc/apns-conf.xml:system/etc/apns-conf.xml
+	vendor/vanir/proprietary/cdma/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/vanir/overlay/cdma_phone
+	vendor/vanir/overlay/cdma_phone
 
 #... and then make the AOSP device configuration for toro get on its knees and sing the national anthem into our "microphones"
 PRODUCT_LOCALES := en_US
+
+# Default values used by 99vanir. copied to /sdcard on first run, and presently it lives there forever... todo: make a runonce script that adds new default values to the copy on the sdcard as we add them or something
+PRODUCT_COPY_FILES += \
+	vendor/vanir/proprietary/jf/etc/vanir.cfg:system/etc/vanir.cfg
 
 # Setup device specific product configuration.
 PRODUCT_NAME := vanir_vigor
