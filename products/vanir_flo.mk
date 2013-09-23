fc2420a1c39c1d6d2dbf58de9fde53fb3d04d9e7
PrimeDirective <activethrasher00@gmail.com>
2013-09-23 14:12:07 -0400

    FLO: use 1280x1920 boot animation
    Change-Id: Ic956c195177846c1f5176a8eaf4d3ff44c00d90c

--- products/vanir_flo.mk
+++ products/vanir_flo.mk
@@ -12,7 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/boot_animations/1280x1920.zip:system/media/bootanimation.zip
 
 # Inherit common product files.
 $(call inherit-product, vendor/vanir/products/common_tabs.mk)
