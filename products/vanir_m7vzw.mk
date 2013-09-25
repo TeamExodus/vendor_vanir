6a5f240ed14a41272c72724aff951a938e89d9b6
DHOMD <DHO@dho.im>
2013-09-25 16:14:48 -0400

    Clean up 4.2.2 tags
    Change-Id: I514ac7fa5128bf2b1536a951c63273a9f2543e7e

--- products/vanir_m7vzw.mk
+++ products/vanir_m7vzw.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/vanir/products/vanir_m7.mk)
 $(call inherit-product, vendor/vanir/products/cdma.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=JZO54K BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JLS36G/221778.8:user/release-k" PRIVATE_BUILD_DESC="1.10.605.8 CL221778 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=JZO54K BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.3/JLS36G/221778.8:user/release-k" PRIVATE_BUILD_DESC="1.10.605.8 CL221778 release-keys"
 
 PRODUCT_NAME := vanir_m7vzw
 PRODUCT_DEVICE := m7vzw
