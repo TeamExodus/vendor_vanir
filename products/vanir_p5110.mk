9a717b56b5f74c4862397f8300ea44433aa5abeb
DHOMD <DHO@dho.im>
2013-10-10 15:48:41 -0400

    Tab 2 mks
    Change-Id: I6194d2d10de0e7bea54714c59372c7923fe2c05f

---
+++ products/vanir_p5110.mk
@@ -0,0 +1,30 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+# Run these first or they will not stick
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-p5110
+
+# Inherit VANIR files.
+$(call inherit-product, vendor/vanir/products/gsm.mk)
+$(call inherit-product, vendor/vanir/products/common_tabs.mk)
+$(call inherit-product, vendor/vanir/products/beats.mk)
+
+# Copy VANIR files
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/600x1024.zip:system/media/bootanimation.zip
+
+# Inherit VANIR overlays.
+    PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_radio
+
+# Inherit AOSP device configuration for Tab 2 10.0 WiFi + GSM
+$(call inherit-product, device/samsung/p5110/full_p5110.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := vanir_p5110
+PRODUCT_DEVICE := p5110
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-P5110
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=espresso10wifixx TARGET_DEVICE=espresso10wifi BUILD_FINGERPRINT=samsung/espresso10wifixx/espresso10wifi:4.3.1/JLS36I/P5110XWALD2:user/release-keys PRIVATE_BUILD_DESC="espresso10wifixx-user 4.3.1 JLS36I P5110XWALD2 release-keys"
