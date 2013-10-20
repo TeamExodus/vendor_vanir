06360e843d6c9420d4bbc2d3d2d568d9ff5310a6
DHOMD <DHO@dho.im>
2013-10-20 19:47:58 -0400

    Fix up p700 mk
    Change-Id: I4d1b71dceb9ce1746ebe4c08a7e5f96980e1167d

--- products/vanir_p700.mk
+++ products/vanir_p700.mk
@@ -6,8 +6,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-p700
 
 # place boot animation here once we make one
-# PRODUCT_COPY_FILES += \
-#    vendor/vanir/proprietary/boot_animations/480x480.zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/480x480.zip:system/media/bootanimation.zip
 
 # Inherit common product files.
 $(call inherit-product, vendor/vanir/products/gsm.mk)
@@ -24,4 +24,4 @@ PRODUCT_DEVICE := p700
 PRODUCT_MODEL := LG-P700
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u0_open_eu BUILD_FINGERPRINT=lge/u0_open_eu/u0:4.0.3/IML74K/lgp700-V10a.20120418.144808:user/release-keys PRIVATE_BUILD_DESC="u0_open_eu-user 4.0.3 IML74K lgp700-V10a.20120418.144808 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u0_open_eu BUILD_FINGERPRINT=lge/u0_open_eu/u0:4.3.1/JLS36I/lgp700-V10a.20120418.144808:user/release-keys PRIVATE_BUILD_DESC="u0_open_eu-user 4.3.1 JLS36I lgp700-V10a.20120418.144808 release-keys"
