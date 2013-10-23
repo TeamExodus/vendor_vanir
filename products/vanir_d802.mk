28089afac97fbace103cbb87014305852e69e001
DHOMD <DHO@dho.im>
2013-10-22 21:58:26 -0400

    Fix up vanir_d802
    Change-Id: I2410529b0ff026667b4e07e0f36f7bf57740642f

--- products/vanir_d802.mk
+++ products/vanir_d802.mk
@@ -8,9 +8,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Inherit device configuration
 $(call inherit-product, device/lge/d802/d802.mk)
 
+# Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/vanir_g2-common.mk)
-
 $(call inherit-product, vendor/vanir/products/cdma.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d802
 PRODUCT_NAME := vanir_d802
@@ -18,4 +19,4 @@ PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D802
 PRODUCT_MANUFACTURER := lge
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_open_com/g2:4.2.2/JDQ39B/D80210a.1378316352:user/release-keys PRIVATE_BUILD_DESC="g2_open_com-user 4.2.2 JDQ39B D80210a.1378316352 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_open_com/g2:4.3.1/JZO54K/D80210a.1378316352:user/release-keys PRIVATE_BUILD_DESC="g2_open_com-user 4.3.1 JZO54K D80210a.1378316352 release-keys"
