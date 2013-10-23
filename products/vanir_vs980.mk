c181715424d2251767081ec33675f47a9d836594
DHOMD <DHO@dho.im>
2013-10-22 22:00:31 -0400

    Fix up vanir_vs980
    Change-Id: If8953722b0f84330ee80385b73034dc098cb8b24

--- products/vanir_vs980.mk
+++ products/vanir_vs980.mk
@@ -8,8 +8,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Inherit device configuration
 $(call inherit-product, device/lge/vs980/vs980.mk)
 
+# Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/vanir_g2-common.mk)
-
 $(call inherit-product, vendor/vanir/products/cdma.mk)
 
 ## Device identifier. This must come after all inclusions
@@ -18,4 +18,4 @@ PRODUCT_NAME := vanir_vs980
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := VS980 4G
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_vzw/g2:4.2.2/JDQ39B/VS98011A.1378346052:user/release-keys PRIVATE_BUILD_DESC="g2_vzw-user 4.2.2 JDQ39B VS98011A.1378346052 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_vzw/g2:4.3.1/JLS36I/VS98011A.1378346052:user/release-keys PRIVATE_BUILD_DESC="g2_vzw-user 4.3.1 JLS36I VS98011A.1378346052 release-keys"
