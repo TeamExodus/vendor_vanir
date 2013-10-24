fe82ac74516448d4db87bd96ad11c57f5e168070
nuclearmistake <nuclearmistake@gmail.com>
2013-10-24 09:14:48 -0400

    ATT G2 (d800)
    Change-Id: I7ca9c7d8cdc565a1ee8e2b93db99ca0735847083

---
+++ products/vanir_d800.mk
@@ -0,0 +1,22 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.goo.rom=vanir-d800
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d800/d800.mk)
+
+# Inherit common vanir files.
+$(call inherit-product, vendor/vanir/products/vanir_g2-common.mk)
+$(call inherit-product, vendor/vanir/products/gsm.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d800
+PRODUCT_NAME := vanir_d800
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D800
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_att_us/g2:4.3.1/JZO54K/D80010d.1376460177:user/release-keys PRIVATE_BUILD_DESC="g2_att_us-user 4.3.1 JZO54K D80010d.1376460177 release-keys"
