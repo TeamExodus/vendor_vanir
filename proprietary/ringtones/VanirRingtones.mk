77d90543cb4ac35ec6e6a65c0162804c0df11101
MBQ <MBQsniper@hotmail.com>
2013-11-12 23:12:18 -0700

    Add in my ringtones, and set them as defaults. Read more below.
    I was able to reduce the file sizes drastically without hurting quality.
    128kbps at 16-bit float with 32-sinc is fine to me.
    
    5 ringtones at 1.16MB. Better?
    
    Change-Id: Ib7d6549a2b458221fd11ed6f3c37e2ee5e0a748d

---
+++ proprietary/ringtones/VanirRingtones.mk
@@ -0,0 +1,16 @@
+#
+# Audio Package 666
+#
+# MBQ wuz here
+#
+# Include this file in a product makefile, suckah
+#
+
+LOCAL_PATH:= vendor/vanir/proprietary/ringtones
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone1.ogg:system/media/audio/ringtones/VanirRingtone1.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone2.ogg:system/media/audio/ringtones/VanirRingtone2.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone3.ogg:system/media/audio/ringtones/VanirRingtone3.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone2.ogg:system/media/audio/ringtones/VanirRingtone4.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone3.ogg:system/media/audio/ringtones/VanirRingtone5.ogg 
