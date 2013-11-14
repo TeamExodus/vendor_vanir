4d292b47353ca37c8769d5ef5ec1cf9845e9a6e7
nuclearmistake <nuclearmistake@gmail.com>
2013-11-13 21:24:17 -0500

    Update hammerhead makefile
    Change-Id: I12944900ff8f8eab27a606c6887da38247bfc93d

--- products/vanir_hammerhead.mk
+++ products/vanir_hammerhead.mk
@@ -6,29 +6,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-hammerhead
 
 # Inherit VANIR files.
-$(call inherit-product, vendor/vanir/products/gsm.mk)
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
+$(call inherit-product, vendor/vanir/products/gsm.mk)
 
-#
-#
-# UPDATE BOOT ANIMATION SIZE ONCE WE KNOW IT
-#
-#
 # Copy VANIR files
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/768x1280.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
-# Inherit AOSP device configuration for Nexus 5
+# Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := vanir_hammerhead
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_BRAND := Android
+PRODUCT_NAME := vanir_hammerhead
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# Product key is based on mako, will need update
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/hammerhead:4.4/KRT16M/737497:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.4 KRT16M 737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
