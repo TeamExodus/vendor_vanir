f07799f55e506704d490c1e9cffd0e0a8551d15f
DHOMD <DHO@dho.im>
2013-12-17 18:13:33 -0500

    Update xt907 + 1060 fingerprints + going OCD on mk files
    Change-Id: I82f82ab688d3dacb6fa55e5ec42f5dba6305a756

--- products/vanir_xt907.mk
+++ products/vanir_xt907.mk
@@ -8,25 +8,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Inherit AOSP device configuration for xt907.
 $(call inherit-product, device/motorola/xt907/full_xt907.mk)
 
+NO_DRM_BLOBS := true
+
+# Inherit common Vanir files.
+$(call inherit-product, vendor/vanir/products/beats.mk)
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+$(call inherit-product, vendor/vanir/products/cdma.mk)
+
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/540x960.zip:system/media/bootanimation.zip
-
-# Inherit common product files.
-NO_DRM_BLOBS := true
-$(call inherit-product, vendor/vanir/products/beats.mk)
-
-# Inherit common phone stuff
-$(call inherit-product, vendor/vanir/products/common_phones.mk)
-
-$(call inherit-product, vendor/vanir/products/cdma.mk)
 
 # Setup device specific product configuration.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_BRAND=motorola \
     PRODUCT_NAME=XT907_verizon \
     BUILD_PRODUCT=scorpion_mini \
-    BUILD_FINGERPRINT=motorola/XT907_verizon/scorpion_mini:4.1.2/9.8.1Q-66/28:user/release-keys
+    BUILD_FINGERPRINT=motorola/XT907_verizon/scorpion_mini:4.4.2/9.8.1Q-66/28:user/release-keys
 
 PRODUCT_NAME := vanir_xt907
 PRODUCT_DEVICE := xt907
