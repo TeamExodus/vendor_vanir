89492e6d10ee91e073eb56dc9e68f9997a558580
DHOMD <DHO@dho.im>
2013-12-28 10:19:01 -0500

    d710 never had a proper mk file,... no wonder it's broken!
    Change-Id: Ide9739ab6b3ec0ee3e64fb9d5a0240c0521d234a

--- products/vanir_d710.mk
+++ products/vanir_d710.mk
@@ -1,25 +1,22 @@
-#
-# Copyright (C) 2012 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#Squisher Choosing
+DHO_VENDOR := vanir
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.goo.rom=vanir-d710
 
-
+# Inherit device configuration
 $(call inherit-product, device/samsung/d710/full_d710.mk)
+
+# Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/cdma.mk)
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d710
 PRODUCT_NAME := vanir_d710
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SPH-D710 
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D710 TARGET_DEVICE=SPH-D710 BUILD_FINGERPRINT=samsung/SPH-D710/SPH-D710:4.4.2/JZO54K/SPH-D710.GB27:user/release-keys PRIVATE_BUILD_DESC="SPH-D710-user 4.4.2 JZO54K SPH-D710.GB27 release-keys"
