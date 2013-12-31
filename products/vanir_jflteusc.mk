2e83d3c1af1b7051c7dabb3a7178199d46ef40d8
DHOMD <DHO@dho.im>
2013-12-30 21:35:57 -0500

    Update jflteusc prop
    Change-Id: I13ed94c87e8f3133e56800a24857e2e2ff966e19

--- products/vanir_jflteusc.mk
+++ products/vanir_jflteusc.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/vanir/products/vanir_jf-common.mk)
 
 $(call inherit-product, vendor/vanir/products/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMD9:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMD9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.4.2/KOT49H/R970VXUAMD9:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.4.2 KOT49H R970VXUAMD9 release-keys"
 
 PRODUCT_NAME := vanir_jflteusc
 PRODUCT_DEVICE := jflteusc
