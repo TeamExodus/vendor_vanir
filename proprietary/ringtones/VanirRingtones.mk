b46f0e9aba7d8592c90e05264826b54845261927
nuclearmistake <nuclearmistake@gmail.com>
2014-01-06 19:30:41 -0500

    VanirRingtones.mk: kill deleted copyfiles lines, and KILL TABS GRRRRRRR
    Change-Id: I3986e690bebc616c70b78707163709ed68c14444

--- proprietary/ringtones/VanirRingtones.mk
+++ proprietary/ringtones/VanirRingtones.mk
@@ -9,8 +9,6 @@
 LOCAL_PATH:= vendor/vanir/proprietary/ringtones
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone1.ogg:system/media/audio/ringtones/VanirRingtone1.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone2.ogg:system/media/audio/ringtones/VanirRingtone2.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone3.ogg:system/media/audio/ringtones/VanirRingtone3.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone2.ogg:system/media/audio/ringtones/VanirRingtone4.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/VanirRingtone3.ogg:system/media/audio/ringtones/VanirRingtone5.ogg 
+    $(LOCAL_PATH)/ringtones/ogg/VanirRingtone1.ogg:system/media/audio/ringtones/VanirRingtone1.ogg \
+    $(LOCAL_PATH)/ringtones/ogg/VanirRingtone2.ogg:system/media/audio/ringtones/VanirRingtone2.ogg \
+    $(LOCAL_PATH)/ringtones/ogg/VanirRingtone3.ogg:system/media/audio/ringtones/VanirRingtone3.ogg
