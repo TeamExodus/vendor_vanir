b69d7f73c9e3cd22c6ed8999e4b2cdc0eaed8b9a
nuclearmistake <nuclearmistake@gmail.com>
2014-01-21 21:41:48 -0500

    Revert "Force disable dex preopt"
    This reverts commit af0ab27d677957279bc9e81aefe8af6c889b8957.

--- products/common.mk
+++ products/common.mk
@@ -3,9 +3,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/common
 #optional theme files
 PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/theme
 
-# Disable dex preopt
-DISABLE_DEXPREOPT := true
-
 # DSPManager and bravia
 $(call inherit-product, vendor/vanir/products/media_sexificators.mk)
 $(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
