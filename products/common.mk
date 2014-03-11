65ce08dc07adee1a0effdd234ce0e37ff2c9bad0
nuclearmistake <nuclearmistake@gmail.com>
2014-03-11 09:53:24 -0400

    Update fingerprints
    Change-Id: Ibce2425c9a2f560005ed40cf159bcec3350683cd

--- products/common.mk
+++ products/common.mk
@@ -115,7 +115,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #    profiler.force_disable_ulog=1 \
 
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=KOT49H BUILD_ID=KOT49H BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=KOT49L BUILD_ID=KOT49L BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
 
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
--- products/vanir_grouper.mk
+++ products/vanir_grouper.mk
@@ -32,4 +32,4 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4.2/KOT49L/937116:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49L 937116 release-keys"
