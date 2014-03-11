46a7affb5ccc8ad60210d4fcbabc6256367b8649
nuclearmistake <nuclearmistake@gmail.com>
2014-03-11 09:58:49 -0400

    p880: reconcile with cm
    Change-Id: Ie463198e3cfc325b44c555fc6865dca2471ea557

--- products/vanir_p880.mk
+++ products/vanir_p880.mk
@@ -16,12 +16,14 @@ $(call inherit-product, vendor/vanir/products/beats.mk)
 # Inherit CM device configuration for p880.
 $(call inherit-product, device/lge/p880/p880.mk)
 
-# Setup device specific product configuration.
-PRODUCT_NAME := vanir_p880
+## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := Optimus4XHD
 PRODUCT_DEVICE := p880
+PRODUCT_NAME := vanir_p880
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := Optimus 4X HD
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x3_open_eu BUILD_FINGERPRINT=lge/x3_open_eu/x3:4.4.2/KOT49L/P88020a.1e56bb4c69:user/release-keys PRIVATE_BUILD_DESC="x3_open_eu-user 4.4.2 KOT49L P88020a.1e56bb4c69 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x3_open_eu BUILD_FINGERPRINT=lge/x3_open_eu/x3:4.1.2/JZO54K/P88020a.1e56bb4c69:user/release-keys PRIVATE_BUILD_DESC="x3_open_eu-user 4.1.2 JZO54K P88020a.1e56bb4c69 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
