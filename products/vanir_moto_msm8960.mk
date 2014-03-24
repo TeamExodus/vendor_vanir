afd90737a718bf0b09e263192491e539243fbddd
nuclearmistake <nuclearmistake@gmail.com>
2014-03-24 10:18:17 -0400

    moto_msm8960: correct boot ani
    Change-Id: I61fb9a6e99ff7c821f4655b8e4097b5be3de37c2

--- products/vanir_moto_msm8960.mk
+++ products/vanir_moto_msm8960.mk
@@ -6,7 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/boot_animations/540x960.zip:system/media/bootanimation.zip
 # Inherit common phone stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
