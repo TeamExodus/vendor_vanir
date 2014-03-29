b458960b4cf1885a4b3f5e34e86e258296ae3872
nuclearmistake <nuclearmistake@gmail.com>
2014-03-29 13:50:48 -0400

    jflte: move initial application of nuclearwinter to conditionally kill mpdecision and thermal crap later in boot
    Change-Id: Idbbe8892da03b5a4b43945cb8ed09cde56f12eae

--- products/vanir_jflte.mk
+++ products/vanir_jflte.mk
@@ -15,7 +15,7 @@ PRODUCT_COPY_FILES += \
 
 # MPDecision and thermal(d/-engine) auto-disablers for KT-based kernels (like ours)
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/jf/etc/init.d/00kernelcompat:system/etc/init.d/00kernelcompat \
+    vendor/vanir/proprietary/jf/etc/boot.d/69nuclearwinter:system/etc/boot.d/69nuclearwinter \
     vendor/vanir/proprietary/jf/bin/nuclearwinter:system/bin/nuclearwinter \
     vendor/vanir/proprietary/jf/bin/nuclearwinterinternal:system/bin/nuclearwinterinternal
 
