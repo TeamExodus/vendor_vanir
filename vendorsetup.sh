af670f7dccd44cbe7e0748f0d8712807afff45fe
nuclearmistake <nuclearmistake@gmail.com>
2014-04-06 12:14:56 -0400

    vendorsetup.sh: alphabetize ville, and auto-sort in case ordering slacks in the future
    Change-Id: I4316d8e23dc1ae8ee86af218534c48bae5ec256c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
-ZOMG_ITS_A_COMBO_COMBO()
+COMBOS()
 {
-  for x in \
-    anzu \
-    coconut \
-    codinalte \
-    deb \
-    d2lte \
-    d710 \
-    d800 \
-    d801 \
-    d802 \
-    d803 \
-    dlx \
-    e980 \
-    flo \
-    galaxysmtd \
-    geeb \
-    gee \
-    golden \
-    grouper \
-    haida \
-    hallon \
-    hammerhead \
-    hlte \
-    i9100g \
-    i605 \
-    i9300 \
-    i9305 \
-    iyokan \
-    jewel \
-    ville \
-    jflte \
-    ls980 \
-    m7att \
-    m7spr \
-    m7tmo \
-    m7ul \
-    m7vzw \
-    maguro \
-    mango \
-    manta \
-    mako \
-    maserati \
-    moto_msm8960 \
-    moto_msm8960dt \
-    nicki \
-    p880 \
-    pollux \
-    pollux_windy \
-    satsuma \
-    skomer \
-    smultron \
-    t0lteatt \
-    t0ltetmo \
-    tilapia \
-    toro \
-    toroplus \
-    urushi \
-    vs980 \
-    yuga;
-  do
-    add_lunch_combo vanir_$x-$1
-  done
+cat<<EOF
+hammerhead
+anzu
+coconut
+codinalte
+deb
+d2lte
+d710
+d800
+d801
+d802
+d803
+dlx
+e980
+flo
+galaxysmtd
+geeb
+gee
+golden
+grouper
+haida
+hallon
+hlte
+i9100g
+i605
+i9300
+i9305
+iyokan
+jewel
+jflte
+ls980
+m7att
+m7spr
+m7tmo
+m7ul
+m7vzw
+maguro
+mango
+manta
+mako
+maserati
+moto_msm8960
+moto_msm8960dt
+nicki
+p880
+pollux
+pollux_windy
+satsuma
+skomer
+smultron
+t0lteatt
+t0ltetmo
+tilapia
+toro
+toroplus
+urushi
+ville
+vs980
+yuga
+EOF
 }
 
-ZOMG_ITS_A_COMBO_COMBO userdebug
+for x in `COMBOS | sort -h`; do 
+  add_lunch_combo vanir_$x-userdebug
+done
