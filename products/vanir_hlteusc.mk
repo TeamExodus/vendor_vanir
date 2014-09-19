113b4b0353e3b4e32fcfaf240a9f9ca83d8c5053
nuclearmistake <nuclearmistake@gmail.com>
2014-09-19 09:08:42 -0400

    hlteusc: copypasta

--- products/vanir_hlteusc.mk
+++ products/vanir_hlteusc.mk
@@ -10,7 +10,7 @@ PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/hltespr/full_hlteusc.mk)
+$(call inherit-product, device/samsung/hlteusc/full_hlteusc.mk)
 
 # Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
