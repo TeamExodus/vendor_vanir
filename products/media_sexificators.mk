0619e54a4b01403ba6881c8ac79c482431c8c13f
nuclearmistake <nuclearmistake@gmail.com>
2014-09-28 10:43:15 -0400

    [DO NOT MERGE] kill bravia
    Change-Id: Idda5f6a4cbd428f4c559df67104fb35c45334244

--- products/media_sexificators.mk
+++ products/media_sexificators.mk
@@ -2,17 +2,3 @@ PRODUCT_PACKAGES += \
     audio_effects.conf \
     DSPManager \
     libcyanogen-dsp
-
-# Bravia 3 engine, credit C0deF0x
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/bravia/etc/be_movie:system/etc/be_movie \
-    vendor/vanir/proprietary/bravia/etc/be_photo:system/etc/be_photo \
-    vendor/vanir/proprietary/bravia/etc/permissions/com.sonyericsson.android.SwIqiBmp.xml:system/etc/permissions/com.sonyericsson.android.SwIqiBmp.xml \
-    vendor/vanir/proprietary/bravia/framework/com.sonyericsson.android.SwIqiBmp.jar:system/framework/com.sonyericsson.android.SwIqiBmp.jar
-
-# Build.Prop Tweaks
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.service.swiqi3.enable=1 \
-    ro.service.swiqi3.supported=true \
-    persist.service.swiqi.enable=1 \
-    ro.service.swiqi.supported=true
