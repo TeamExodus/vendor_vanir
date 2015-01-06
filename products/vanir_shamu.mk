65899d96c5197323f422bdc4e93db02baffcdcc7
nuclearmistake <nuclearmistake@gmail.com>
2015-01-06 09:07:59 -0500

    Cleanup vanir_shamu.mk
    Change-Id: Ib83048152d1c693c2f1f9d8b846c8b94c3406e74

--- products/vanir_shamu.mk
+++ products/vanir_shamu.mk
@@ -18,13 +18,10 @@ $(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := shamu
 PRODUCT_NAME := vanir_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
-PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT=google/shamu/shamu:5.0.1/LRX22G/1602158:user/release-keys PRIVATE_BUILD_DESC="shamu-user 5.0.1 LRX22G 1602158 release-keys"
-
-# Enable Torch
-PRODUCT_PACKAGES += Torch
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu BUILD_FINGERPRINT=google/shamu/shamu:5.0.1/LRX22C/1602158:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.0.1 LRX22C 1602158 release-keys"
