bb6192bb680a013632ee5c29a352a413b45ce0ba
ench0 <ench0@users.noreply.github.com>
2015-01-12 16:55:49 +0000

    Update and rename vanir_hltetmo.mk to vanir_hlte.mk

--- products/vanir_hlte.mk
+++ products/vanir_hlte.mk
@@ -3,18 +3,18 @@ DHO_VENDOR := vanir
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
-    ro.goo.rom=vanir-hltetmo
+    ro.goo.rom=vanir-hlte
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
+$(call inherit-product, device/samsung/hltetmo/full_hlte.mk)
 
 # Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hltetmo
-PRODUCT_NAME := vanir_hltetmo
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := vanir_hlte
