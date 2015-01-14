e3a231ffa256f788d260f9614088677e66a675a7
nuclearmistake <nuclearmistake@gmail.com>
2015-01-14 08:29:47 -0500

    ZOMG @DHO COPY PASTA ZOMG :-P
    Change-Id: I4684937218603c39ac6014697766831e2c1d9217

--- products/vanir_d2att.mk
+++ products/vanir_d2att.mk
@@ -1,16 +1,16 @@
 #Squisher Choosing
-DHO_VENDOR := commotio
+DHO_VENDOR := vanir
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.rom=commotio-d2att
+    ro.goo.rom=vanir-d2att
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/commotio/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
 
 # Inherit common vanir stuff
-$(call inherit-product, vendor/commotio/products/common_phones.mk)
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
 
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
-PRODUCT_NAME := commotio_d2att
+PRODUCT_NAME := vanir_d2att
