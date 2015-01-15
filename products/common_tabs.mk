7686419697766131eb54b202321b5c7003ce0b02
Dave Kessler <activethrasher00@gmail.com>
2015-01-15 17:47:24 -0500

    common_tabs.mk should inherit the lollipop sounds leaving AOSP behind
    Change-Id: I849d4e14e62d68b13a724991855ab0e073b895d9

--- products/common_tabs.mk
+++ products/common_tabs.mk
@@ -1,5 +1,8 @@
 # Vanir common
 $(call inherit-product, vendor/vanir/products/common.mk)
 
-# Audio Packages
-include frameworks/base/data/sounds/AudioPackage7alt.mk
+# AOSP audio packages from KK44-AOSP
+# include frameworks/base/data/sounds/AudioPackage7alt.mk
+
+# Lollipop sounds pulled from N9 system image
+$(call inherit-product-if-exists, frameworks/base/data/sounds/lollipop-tablet/AllExodusAudioTablet.mk)
