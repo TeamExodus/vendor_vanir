339f43e3060fdbbca26b6863bcf0ad2690631f82
Dave Kessler <activethrasher00@gmail.com>
2015-03-16 15:29:35 -0400

    add FlounderLTE
    Change-Id: I69c7d9178959f2230e8d9d9019932ec3f6142f5f

--- products/vanir_flounderlte.mk
+++ products/vanir_flounderlte.mk
@@ -7,24 +7,26 @@ KERNEL_SPAM := MODIFIED-AOSP 3.10.0 Prebuilt
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-FLOUNDER
 
-# Tablet Overlays no radios
-    PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_no_radio
+# Tablet Overlays with radios
+    PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_radio
+
+# GSM APNs and Simcard app
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/832x520.zip:system/media/bootanimation.zip
 
-# Inherit AOSP device configuration for grouper
-$(call inherit-product, device/htc/flounder/full_flounder.mk)
+$(call inherit-product, device/htc/flounderlte/full_flounderlte.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/vanir/products/common_tabs.mk)
 
+
 # Setup device specific product configuration.
-PRODUCT_NAME := vanir_flounder
+PRODUCT_NAME := vanir_flounderlte
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := flounder
 PRODUCT_MODEL := Nexus 9
-PRODUCT_MANUFACTURER := HTC
+PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=volantis TARGET_DEVICE=flounder BUILD_FINGERPRINT=google/volantis/flounder:5.0/LRX21L/1546449:user/release-keys PRIVATE_BUILD_DESC="volantis-user 5.0 LRX21L 1546449 release-keys"
